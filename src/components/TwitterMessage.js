f001aa743c8067cc3c256b034ed22a05d0f94abb
github username <josephtrettevik@gmail.com>
2019-10-17 09:56:57 -0700

    Added a onChange handler and made Your Message a controlled from

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,36 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(abc) {
     super();
 
-    this.state = {};
+    this.state = {
+      message: "",
+      remainingChars: abc.maxChars
+    };
+  }
+
+  handleMessageChange = (e) => {
+    console.log(e.target.value)
+    const newMessage = e.target.value;
+    const newRemainingChars = this.state.remainingChars - newMessage.split('').length
+    this.setState({
+      message: newMessage,
+      remainingChars: newRemainingChars
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text" 
+          name="message"
+          onChange={this.handleMessageChange} 
+          value={this.state.message} 
+          id="message" 
+        />
       </div>
     );
   }
